3a51fc282e0ed3e37f55504d938258b8feffae1c
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-04-07 09:40:21 +0530

    Changed the home link text

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header>
-          <Link to="/">Home</Link> <br />
+          <Link to="/">Home Link</Link> <br />
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
